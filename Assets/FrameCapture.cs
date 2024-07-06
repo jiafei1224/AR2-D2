cde45c2655cf0e6c72c0628f849502240ab3d134
Jiafei Duan <51585075+jiafei1224@users.noreply.github.com>
2024-07-06 10:21:12 -0700

    Add files via upload

---
+++ Assets/FrameCapture.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.iOS;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Rendering;
+
+using NativeGalleryNamespace;
+
+public class FrameCapture : MonoBehaviour
+{
+   
+}
